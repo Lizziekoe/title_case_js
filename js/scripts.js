0bed34a69fffcc89419f67e9dd51701c79b0128e
Lizzie Koehler and Mike Goren <mgoren@mortalwombat.net>
2015-02-10 10:24:52 -0800

    add jquery and html

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,11 @@ var titleCase = function(phrase){
 
   var newPhrase = "";
   var connectingWords = ['a', 'of', 'the', 'to', 'and', 'by', 'or', 'is']
+  var timeThroughLoop = 1;
 
   var words = phrase.split(" ");
   words.forEach(function(word) {
-    if (connectingWords.indexOf(word) === -1) {
+    if (connectingWords.indexOf(word) === -1 || timeThroughLoop === 1) {
       var firstLetter = word[0].toUpperCase();
       var remainingLetters = word.slice(1);
       var newWord = firstLetter + remainingLetters;
@@ -13,9 +14,24 @@ var titleCase = function(phrase){
     } else {
       newPhrase = newPhrase + word + " ";
     }
+    timeThroughLoop += 1;
   });
 
   newPhrase = newPhrase.slice(0, newPhrase.length - 1);
   return newPhrase;
 
 };
+
+$(document).ready(function() {
+
+  $("form#titleForm").submit(function(event) {
+
+    var title = $("input#title").val();
+    var newTitle = titleCase(title);
+
+    $("#results p").text(newTitle)
+
+    event.preventDefault();
+  });
+
+});
--- spec/specs.js
+++ spec/specs.js
@@ -10,4 +10,8 @@ describe('titleCase', function() {
   it('does not capitzlize a, of, the, to, and, by & or', function() {
     expect(titleCase("this is a crazy cat or dog")).to.equal("This is a Crazy Cat or Dog");
   });
+
+  it('does capitalize connecting words if is first word', function() {
+    expect(titleCase("the crazy cat is on your head")).to.equal("The Crazy Cat is On Your Head");
+  });
 });
